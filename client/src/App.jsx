9eb4896074eb1270ab7f688de70bf4c97a1663c2
Britta Jorgenson <bljorgen@gmail.com>
2019-06-22 23:57:43 +0000

    create a basic footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import Footer from './Footer.jsx';
 
 class App extends Component {
-
   render(){
     return (
-      <div>Hello World</div>
+      <div>
+        <div>Hello World</div>
+        <Footer/>
+      </div>
     );
   }
 }
