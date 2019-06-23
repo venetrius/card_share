618c3392fb285434b52a38a349166d0d70167730
venetrius <gergelygjuhasz@gmail.com>
2019-06-23 17:16:37 +0000

    add socket with simple example chat

--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,27 @@
 "use strict";
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
-require('dotenv').config();
+//const bodyParser    = require("body-parser");
+const app           = require('express')();
+const http          = require('http').Server(app);
+const io            = require('socket.io')(http);
+                      require('dotenv').config();
+const PORT          = process.env.PORT || 8080;
 
 
-app.get('/', function (req, res) {
-  res.send(`
-    <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Hi</title>
-      </head>
-      
-      <body>
-       Hello World
-      </body>
-    
-    </html> 
-  `);
-})
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-const server = app.listen(process.env.PORT || PORT, () => {
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg)
+    {
+      io.emit('chat message', msg);
+    }
+  );
+});
 
-  console.log("Example app listening on port " + (process.env.PORT || PORT));
-
+const server = http.listen(PORT, () => {
+  console.log("Server is listening on port " + (PORT));
 });
 
 // it is used by Mocha
