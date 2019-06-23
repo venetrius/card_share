ad6aff8aa09ea6b45a3c9bf8f390fa11d05e69ce
venetrius <gergelygjuhasz@gmail.com>
2019-06-23 12:53:41 -0600

    connect client to socket server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
+import socketIOClient from "socket.io-client";
 import Footer from './Footer.jsx';
 import Header from './Header.jsx';
 import Main from './Main.jsx';
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "http://127.0.0.1:8000"
+    };
+  }
+
+  sendAlert(msg){
+    alert(msg);
+  }
+  
+  componentDidMount() {
+    const App = this;
+    const { endpoint } = this.state;
+    let socket = socketIOClient(this.state.endpoint);
+    socket.on('chat message', function(msg){
+      App.sendAlert(msg);
+    });
+  }
+
   render(){
     return (
       <div>
--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const app           = require('express')();
 const http          = require('http').Server(app);
 const io            = require('socket.io')(http);
                       require('dotenv').config();
-const PORT          = process.env.PORT || 8080;
+const PORT          = process.env.PORT || 8000;
 
 
 app.get('/', function(req, res){
