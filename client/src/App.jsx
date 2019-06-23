395a9c493e3055c5af6a2448c020b701f68a308e
Britta Jorgenson <bljorgen@gmail.com>
2019-06-23 00:00:59 +0000

    create a basic header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import Footer from './Footer.jsx';
+import Header from './Header.jsx';
 
 class App extends Component {
   render(){
     return (
       <div>
+        <Header/>
         <div>Hello World</div>
         <Footer/>
       </div>
