e8d8eaf024f36d2b0683032a9f821f8792f28982
Britta Jorgenson <bljorgen@gmail.com>
2019-06-23 00:06:34 +0000

    create a basic main component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import Footer from './Footer.jsx';
 import Header from './Header.jsx';
+import Main from './Main.jsx';
 
 class App extends Component {
   render(){
     return (
       <div>
         <Header/>
-        <div>Hello World</div>
+        <Main/>
         <Footer/>
       </div>
     );
---
+++ client/src/Main.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+
+  render(){
+    return (
+      <p>Hello from Main!</p>
+    );
+  }
+}
+
+export default Main;
