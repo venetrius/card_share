e5777ae9d9228f46d3c2d57dda5b0a59031dc495
venetrius <gergelygjuhasz@gmail.com>
2019-06-24 20:17:22 +0000

    add datahelper "createUser"

--- server/src/util/data_helpers/data-helpers-users.js
+++ server/src/util/data_helpers/data-helpers-users.js
@@ -3,15 +3,31 @@ module.exports = function(knex){
   function getUserById(userId, cb) {
     knex.select('*').from('users')
       .where('id', userId)
-      .asCallback(function(err, userInfo) {
+      .asCallback(function(err, userInfo) 
+      {
         if (err) {
           throw err;
         }
         cb(null, userInfo[0])
-      });
+      }
+    );
+  }
+
+  function createUser(data, cb) {
+    knex('users')
+    .insert(data)
+    .returning('*')
+    .asCallback((err, user)=> 
+    {
+      if (err) {
+        throw err;
+      }
+      cb(null, user);
+    });
   }
 
   return {
-    getUserById
+    getUserById,
+    createUser
   }  
 }
