97d6b86092e019ea18fde0a5a0dea97c46e2ce5e
Britta Jorgenson <bljorgen@gmail.com>
2019-06-26 03:21:09 +0000

    create backend event handlers for card actions

--- server/src/socket/events.js
+++ server/src/socket/events.js
@@ -40,8 +40,59 @@ const get_attendees = function(id) {
   })
 };
 
+// ----------------------------------------------
+//              CARD EVENTS
+// ----------------------------------------------
+
+const request_connection = function(user_id){
+  const socket = this;
+  const client_id = socket.id
+  console.log(`${client_id} requested a connection with ${user_id}`)
+};
+
+const accept_connection = function(user_id){
+  const socket = this;
+  const client_id = socket.id
+  console.log(`${client_id} accepted a connection with ${user_id}`)
+};
+
+const ignore_connection = function(user_id){
+  const socket = this;
+  const client_id = socket.id
+  console.log(`${client_id} ignored a connection with ${user_id}`)
+};
+
+const send_contact = function(user_id){
+  const socket = this;
+  const client_id = socket.id
+  console.log(`${client_id} sent contact to ${user_id}`)
+};
+
+const save_contact = function(user_id){
+  const socket = this;
+  const client_id = socket.id
+  console.log(`${client_id} saved contact of ${user_id}`)
+};
+
+const delete_contact = function(user_id){
+  const socket = this;
+  const client_id = socket.id
+  console.log(`${client_id} deleted contact of ${user_id}`)
+};
+
+
+// ----------------------------------------------
+//              Exports Module
+// ----------------------------------------------
+
   module.exports = {
     get_categories,
     get_user,
-    get_attendees
+    get_attendees,
+    request_connection,
+    accept_connection,
+    ignore_connection,
+    send_contact,
+    save_contact,
+    delete_contact
   };
