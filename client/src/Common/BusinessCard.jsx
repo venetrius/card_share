2991d0718edd0a1c23d3a6aa7ac90d7c73f10606
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-26 14:27:06 -0600

    update contact card format

--- client/src/Common/BusinessCard.jsx
+++ client/src/Common/BusinessCard.jsx
@@ -7,16 +7,72 @@ class BusinessCard extends Component {
   render(){
     return (
       <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>First Last</Card.Title>
-        <Card.Text>
-          This is my tagline!!
-        </Card.Text>
-        <Button variant="primary"> + Connect</Button>
-      </Card.Body>
-    </Card>
+        <Card.Header>
+          <Card.Title>Mickey Mouse</Card.Title>
+
+        </Card.Header>
+      
+
+
+        <Card.Body>
+
+          <Card.Text>
+            This is my tagline!!
+          </Card.Text>
+
+        </Card.Body>
+
+
+
+
+        <Card.Footer>
+          <Button variant="primary"> Send Contact</Button>
+          <Button variant="primary"> + Connect</Button>
+        </Card.Footer>
+      </Card>
     );
   }
 }
 
 export default BusinessCard;
+
+
+{/* <div class="contact-card">
+  <div class="card-header">
+    <span class="name">Mickey Mouse</span>
+    <span class="icons">@ :) :(</span>
+  </div>
+
+  <div class="card-body">
+    <div class="body-top">
+      <img
+        class="photo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl4bUaJNFWtDHNtymDHN23ZZjwNCF7h_TnU6CycbuTQDI45b3i"
+      />
+      <div class="topics">
+        <img src="https://i.stack.imgur.com/CiB0A.png" />
+      </div>
+    </div>
+
+    <div class="body-middle">
+      <p>
+        Title: Actor <br />
+        Company: Disney<br />
+        Phone: 555-555-5555<br />
+        Email: m_mouse@disney.com<br />
+        LinkedIn Button
+      </p>
+    </div>
+
+    <div class="body-bottom">
+      <p>
+        Out of work actor looking for a new show on the Disney Channel
+      </p>
+    </div>
+  </div>
+
+  <div class="card-footer">
+    <button>Sent Contact</button>
+    <button>Connect</button>
+  </div>
+</div> */}
