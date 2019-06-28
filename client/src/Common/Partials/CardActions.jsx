78c90899bf722846f7f6adcf8cfad99c3cf6768f
Britta Jorgenson <bljorgen@gmail.com>
2019-06-28 01:45:02 +0000

    create status depenedent connection action buttons

--- client/src/Common/Partials/CardActions.jsx
+++ client/src/Common/Partials/CardActions.jsx
@@ -2,22 +2,59 @@ import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button'
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
+function connectionActions(attendee_id, connection) {
+  if (!connection) {
+    return (
+      <Button variant="outline-primary" size="sm">Connect</Button>
+    );
+  } else if(connection.status == 'CONNECTED') {
+    return (<span>CONNECTED</span>);
+  } else if(attendee_id == connection.sender) {
+    return (
+      <span>
+        <Button variant="outline-success" size="sm">Accept</Button>
+        <Button variant="outline-danger" size="sm">Ignore</Button>
+      </span>
+    )
+  } else {
+    return (<span>PENDING CONNECTION</span>)
+  }
+}
+
+
+
 class CardActions extends Component {
 
   render(){
+    const attendee = this.props.attendee;
+
     return (
       <ButtonToolbar>
-        <Button variant="outline-primary" size="sm">Send Connection</Button>
-        <Button variant="outline-secondary" size="sm">Secondary</Button>
-        <Button variant="outline-success" size="sm">Contact</Button>
-        <Button variant="outline-warning" size="sm">Warning</Button>
-        <Button variant="outline-danger" size="sm">Danger</Button>
-        <Button variant="outline-info" size="sm">Info</Button>
-        <Button variant="outline-light" size="sm">Light</Button>
-        <Button variant="outline-dark" size="sm">Dark</Button>
+        {connectionActions(attendee.id, attendee.connection)}
+        <Button variant="outline-primary" size="sm">Share Card</Button>
       </ButtonToolbar>
     );
   }
 }
 
 export default CardActions;
+
+{/*
+connection: {"sender":1000002,"status":"SENT"}
+
+---> if(!attendee.connection) {
+  <button onClick={() => this.requestConnection() } > Connect </button>
+  
+  else if(attendee.connection.status != 'CONNECTED')
+  <button onClick={() => this.acceptConnection() } > Accept </button>
+  <button onClick={() => this.ignoreConnection() } > Ignore </button>
+
+}
+
+
+cards: {"from":"PENDING"}
+<button onClick={() => this.sendCard() } > sendCard </button>
+
+<button onClick={() => this.saveCard() } > saveCard </button>
+<button onClick={() => this.deleteCard() } > deleteCard </button> 
+*/}
