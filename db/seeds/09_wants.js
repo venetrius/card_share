623987fbb1abd65c2d3b3b3046bd67c5d1cb9e4c
Christian <cpfeiffer325@gmail.com>
2019-07-01 21:32:44 +0000

    update seed data for testing

--- db/seeds/09_wants.js
+++ db/seeds/09_wants.js
@@ -5,19 +5,17 @@ exports.seed = function(knex, Promise) {
       .then(function () {
         // Inserts seed entries
         return knex('wants').insert([
-          {id: 1000001, attendee_id: 1000001,  sub_category_id: 1000002},
-          {id: 1000002, attendee_id: 1000001,  sub_category_id: 1000005},
-          {id: 1000003, attendee_id: 1000001,  sub_category_id: 1000007},
-          {id: 1000004, attendee_id: 1000001,  sub_category_id: 1000014},
-          {id: 1000005, attendee_id: 1000001,  sub_category_id: 1000020},
+          {id: 1000001, attendee_id: 1000001,  sub_category_id: 1000006},
+          {id: 1000002, attendee_id: 1000001,  sub_category_id: 1000007},
+          {id: 1000003, attendee_id: 1000001,  sub_category_id: 1000008},
+          {id: 1000004, attendee_id: 1000001,  sub_category_id: 1000009},
+          {id: 1000005, attendee_id: 1000001,  sub_category_id: 1000010},
 
-
-          {id: 1000006, attendee_id: 1000002,  sub_category_id: 1000006},
-          {id: 1000007, attendee_id: 1000002,  sub_category_id: 1000010},
-          {id: 1000008, attendee_id: 1000002,  sub_category_id: 1000011},
-          {id: 1000010, attendee_id: 1000002,  sub_category_id: 1000012},
-          {id: 1000011, attendee_id: 1000002,  sub_category_id: 1000018},
-          {id: 1000012, attendee_id: 1000002,  sub_category_id: 1000019},
+          {id: 1000007, attendee_id: 1000002,  sub_category_id: 1000016},
+          {id: 1000008, attendee_id: 1000002,  sub_category_id: 1000017},
+          {id: 1000010, attendee_id: 1000002,  sub_category_id: 1000018},
+          {id: 1000011, attendee_id: 1000002,  sub_category_id: 1000019},
+          {id: 1000012, attendee_id: 1000002,  sub_category_id: 1000020},
 
           {id: 1000013, attendee_id: 1000003,  sub_category_id: 1000008},
           {id: 1000014, attendee_id: 1000003,  sub_category_id: 1000010},
